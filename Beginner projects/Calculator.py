2231545e88c19cfd640c541fb79e2aeed7dafdbb
Opeoluwadigba <opeoluwadigba.idowu@gmail.com>
2022-09-02 15:22:23 +0100

    Create Calculator.py
    A simple calculator program

---
+++ Beginner projects/Calculator.py
@@ -0,0 +1,11 @@
+def addition(a, b):
+    return float(a+b)
+
+def subtraction(a, b):
+    return float(a-b)
+
+def multiplication(a, b):
+    return float(a*b)
+
+def division(a, b):
+    return float(a/b)
